d8d0d4760aaf95b4701b405664d52982a40ab0f2
DLX <lingxiao.dai@luxshare-ict.com>
2026-05-26 16:24:45 +0800

    read and parse words form json file

--- main.py
+++ main.py
@@ -23,6 +23,56 @@ class WordReviewApp(QWidget):
         self.setWindowTitle("Review Words")
         self.json_path = json_path
 
+        self.words_list = []
+        self.current_index = 0
+
+        self.load_data()
+        self.init_ui()
+
+    def init_ui(self):
+        pass
+
+    def load_data(self):
+        """
+        prase json file
+        """
+        try:
+            with open(self.json_path, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+
+            marks = data.get('marks', [])
+            for mark in marks:
+                word = mark.get('text', '')
+                notes_html = mark.get('notes', '')
+
+                # clean html
+                notes_text = self.clean_html__(notes_html) if notes_html else 'Empty Word'
+                if word:
+                    self.words_list.append({
+                        'word': word,
+                        'notes': notes_text
+                    })
+
+            print(f"total words number:{len(self.words_list)}")
+
+        except Exception as e:
+            print(f"Failed to load data: {e}")
+            # QMessageBox.critical(self, "Error", f"Failed to load data: {e}")
+            # return
+
+    def clean_html__(self, html_str):
+        """
+        clean html simply:
+        """
+        # 将 <br> 或 </p><p> 替换为换行符
+        html_str = re.sub(r'<br\s*/?>|</p>\s*<p>', '\n', html_str)
+        # 去除其他所有HTML标签
+        clean_text = re.compile('<.*?>')
+        text = re.sub(clean_text, '', html_str)
+        # 解码常见的HTML实体字符
+        text = text.replace('&lt;', '<').replace('&gt;', '>').replace('&quot;', '"')
+        return text.strip()
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
